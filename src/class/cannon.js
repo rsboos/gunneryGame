7bb0cfd0f90b1c105da6c03e6680be5af235c42b
Rodrigo Boos <rsboos@hotmail.com>
2015-11-25 12:52:50 -0200

    initial rendering

--- src/class/cannon.js
+++ src/class/cannon.js
@@ -5,4 +5,4 @@ objects.Cannon = function Cannon(color,isColider,type,coordinates) {
     this.init(color,isColider,type,coordinates);
    
 }
-objects.Cannon.prototype = Object.create(objects.Fortress.prototype);
+objects.Cannon.prototype = Object.create(objects.PhysicalElement.prototype);
--- src/class/fortress.js
+++ src/class/fortress.js
@@ -3,6 +3,6 @@ if (objects === undefined)
 
 objects.Fortress = function Fortress(color,isColider,type,coordinates) {
     this.init(color,isColider,type,coordinates);
-    this.cannon=true;
+    this.cannon = new objects.Cannon(color,isColider,type,coordinates);
 }
 objects.Fortress.prototype = Object.create(objects.PhysicalElement.prototype);
